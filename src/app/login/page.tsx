6163d8d2da5811643ae2213f4d501f0640b300f7
Ali.r <alidev.r1996@gmail.com>
2025-05-11 13:41:21 +0430

    fix login credential

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,11 +7,13 @@ import SocialLogin from "./social-login";
 import { FormEvent, useRef } from "react";
 import { signIn } from "next-auth/react";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 
 
 const SignIn = () => {
   const formRef = useRef<HTMLFormElement>(null);
+  const router = useRouter();
 
   const creadentialLogin = async (event: FormEvent) => {
     event.preventDefault();
@@ -33,7 +35,7 @@ const SignIn = () => {
       toast.error("نام کاربری یا رمز عبور اشتباه است!");
     } else {
       // login successful: redirect manually
-      window.location.href = "/";
+     router.push("/")
     }
   };
   
