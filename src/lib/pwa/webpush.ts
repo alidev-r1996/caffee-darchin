f35a3aba2848e5dc5efa19bd42eb4f1b8e43fda5
Ali.r <alidev.r1996@gmail.com>
2025-05-23 14:24:10 +0430

    unistall unused packages

--- src/lib/pwa/webpush.ts
+++
@@ -1,9 +0,0 @@
-import webpush from "web-push";
-
-webpush.setVapidDetails(
-  "mailto:your@email.com",
-  process.env.VAPID_PUBLIC_KEY!,
-  process.env.VAPID_PRIVATE_KEY!
-);
-
-export default webpush;
