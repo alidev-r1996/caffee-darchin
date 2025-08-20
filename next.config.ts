49e3ba12cb35d199eab754a50fdf941d57d36eab
Ali.r <alidev.r1996@gmail.com>
2025-08-21 00:40:39 +0430

    add some metadata tag

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,54 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'cafe-darchin.ir' }],
+        destination: 'https://www.cafe-darchin.ir/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
